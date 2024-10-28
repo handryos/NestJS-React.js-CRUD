3f22b3344694cb478fe53d900437fa4db62cc1b3
handryos <handryos12@gmail.com>
2024-10-28 15:08:03 -0300

    ajuste cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://monitoringdyna.netlify.app/',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['content-type'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 8000);
