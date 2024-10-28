57cc56a1633175a4d6568c41f6a781ec8ccb4806
handryos <handryos12@gmail.com>
2024-10-28 15:10:50 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['content-type'],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
