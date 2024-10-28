79969ca9c72f65bc0cff738319ada9aa88ea128d
handryos <handryos12@gmail.com>
2024-10-28 15:49:49 -0300

    ajuste migrations

--- src/prisma/migrations/20241025205833_adjust/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name]` on the table `monitoring_point` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "monitoring_point_name_key" ON "monitoring_point"("name");
